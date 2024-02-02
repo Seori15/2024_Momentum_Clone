872b16df8548bc1661c03085d5c2cb66260600af
Seori15 <wlstn6278@gmail.com>
2024-02-02 23:01:16 +0900

    #8 update Weather feature_02

--- lecture/js/weather.js
+++ lecture/js/weather.js
@@ -1,11 +1,21 @@
-function onGeoOk() {
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in ", lat, lng);
+const API_KEY = "";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:last-child");
+      const weather = document.querySelector("#weather span:first-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
-    alert("Coudln't find your location.");
+  alert("Coudln't find your location.");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
